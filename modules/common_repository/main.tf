14ba0501642ce3307cd495edbbbb7366dec01838
Lars Kellogg-Stedman <lars@redhat.com>
2025-02-04 13:03:47 -0500

    Add org-admins team
    Create an org-admins team, and configure the default branch protection
    rules so that members of this team have force-push access to repositories.

--- modules/common_repository/main.tf
+++ modules/common_repository/main.tf
@@ -36,14 +36,35 @@ resource "github_issue_labels" "repo_labels" {
 resource "github_branch_protection" "repo_protection" {
   # This odd looking construct lets us control the creation of the
   # branch protection resource with a boolean variable.
-  count         = var.branch_protection ? 1 : 0
-  repository_id = var.name
+  count = var.branch_protection ? 1 : 0
 
-  pattern                 = "main"
-  required_linear_history = true
+  repository_id = var.name
+  pattern       = "main"
+
+  required_linear_history         = true
+  allows_deletions                = false
+  allows_force_pushes             = false
+  enforce_admins                  = false
+  require_conversation_resolution = false
+  require_signed_commits          = false
+
+  force_push_bypassers = [
+    "innabox/org-admins",
+  ]
 
   required_pull_request_reviews {
     required_approving_review_count = 1
+  }
+
+  depends_on = [github_repository.repo, github_repository_collaborators.repo_collaborators]
+}
+
+resource "github_repository_collaborators" "repo_collaborators" {
+  repository = var.name
+
+  team {
+    team_id    = "org-admins"
+    permission = "push"
   }
 
   depends_on = [github_repository.repo]
