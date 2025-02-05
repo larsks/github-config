eff7ac35e936e57134d72845f48b72f6daa12f0e
Lars Kellogg-Stedman <lars@redhat.com>
2025-02-04 19:21:26 -0500

    Restrict repository creation
    Require that new repositories are created via this config repository.

--- organization.tf
+++ organization.tf
@@ -4,5 +4,8 @@ resource "github_organization_settings" "innabox" {
   billing_email                                = "lars@redhat.com"
   secret_scanning_enabled_for_new_repositories = true
   has_repository_projects                      = false
-}
 
+  members_can_create_private_repositories = false
+  members_can_create_public_repositories  = false
+  members_can_create_repositories         = false
+}
