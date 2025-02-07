f6334e7a06fd33cbb4d2d849d39f26878369935c
Lars Kellogg-Stedman <lars@redhat.com>
2025-02-06 21:49:24 -0500

    Enable auto merge on repositories

--- modules/common_repository/main.tf
+++ modules/common_repository/main.tf
@@ -1,13 +1,14 @@
 resource "github_repository" "repo" {
-  name          = var.name
-  visibility    = var.visibility
-  description   = var.description
-  auto_init     = true
-  has_issues    = true
-  has_downloads = false
-  has_projects  = false
-  has_wiki      = false
-  is_template   = var.is_template
+  name             = var.name
+  visibility       = var.visibility
+  description      = var.description
+  auto_init        = true
+  allow_auto_merge = true
+  has_issues       = true
+  has_downloads    = false
+  has_projects     = false
+  has_wiki         = false
+  is_template      = var.is_template
 
   dynamic "template" {
     # Use the public_template repository as a template unless the repository is
